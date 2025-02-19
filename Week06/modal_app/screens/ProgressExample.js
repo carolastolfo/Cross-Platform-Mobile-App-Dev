14a203d50782e8ecb27ca74a39f764e44e1632f1
carolastolfo <carolastolfos@gmail.com>
2025-02-19 16:05:34 -0500

    modal practice

--- Week06/modal_app/screens/ProgressExample.js
+++ Week06/modal_app/screens/ProgressExample.js
@@ -7,8 +7,10 @@ import {
     ActivityIndicator, 
     Pressable, 
     TouchableOpacity, 
-    Alert } from "react-native";
+    Alert,
+    Modal } from "react-native";
 import globalStyles from "../shared/GlobalStyles";
+import * as Progress from 'react-native-progress';
 
 /*
 
@@ -19,14 +21,51 @@ npm install react-native-progress
 // modal window presents content above an enclosing view.
 
 const ProgressExample = () => {
+
+    /* 
+        0 = 0%
+        0.4 = 40%
+        0.7 = 70%
+        1.0 = 100%
+    */
+
     const [progress, setProgress] = useState(0.5); //50%
 
-    const handleProgressChange = () => {
+    //variable to manually set visibility of modal window
+    const [showModal, setShowModal] = useState(false)
+
+    const handleProgressChange = (changeValue) => {
         //perform progress change calculation
+        const newValue = progress + changeValue
+
+        if (newValue >= 0 && newValue <=1){
+            setProgress(newValue)
+        }
     };
 
     const showAlert = () => {
-        Alert.alert("Progress Control")
+        Alert.alert(
+            "Progress Control",
+            "This alert shows three diff button",
+            //array of buttons in the alert
+            [
+                {
+                    text: 'Increase',
+                    style: 'default',
+                    onPress: () => {handleProgressChange(0.1)}
+                },
+                {
+                    text: "Decrease",
+                    style: 'destructive',
+                    onPress: () => {handleProgressChange(-0.1)}
+                },
+                {
+                    text: 'Cancel',
+                    style: 'cancel',
+                    onPress: () => console.log(`user cancelled`)
+                }
+            ]
+        )
     }
 
     const progressItems = [
@@ -36,19 +75,53 @@ const ProgressExample = () => {
         },
         { 
             key: "Circle", 
-            component: <Text>Circle</Text>
+            component: <Progress.Circle 
+                            size={80}
+                            progress={progress}
+                            thickness={6}
+                            unfilledColor="grey"
+                            color="magenta"
+                            borderColor="green"
+                            showsText={true}
+                            textStyle={
+                                {
+                                    fontSize:20,
+                                    color: 'blue',
+                                    fontWeight: 'bold'
+                                }
+                            }
+                        />
         },
         { 
             key: "CircleSnail", 
-            component: <Text>Circle Snail</Text>
+            component: <Progress.CircleSnail 
+                            size={80}
+                            thickness={5}
+                            color={['magenta', 'yellow', 'green', 'blue']}
+                            spinDuration={3000} // 3 seconds // default is 5seconds
+                        />
         },
         { 
             key: "Pie", 
-            component: <Text>Pie</Text>
+            component: <Progress.Pie 
+                            size={80}
+                            progress={progress}
+                            thickness={5}
+                            color={'magenta'}
+                            unfilledColor="yellow"
+                        />
         },
         { 
             key: "Progress Bar", 
-            component: <Text>Bar</Text> 
+            component: <Progress.Bar 
+                            width={300}
+                            height={20}
+                            progress={progress}
+                            thickness={5}
+                            color="magenta"
+                            //indeterminate={true}
+                            borderRadius={10}
+                        /> 
         },
     ];
 
@@ -90,26 +163,42 @@ const ProgressExample = () => {
                 style = {globalStyles.button}
                 onPress={() => {
                     //set the showModal to true to show modal window
+                    //or set it up like below to toggle
+                    setShowModal(!showModal)
                 }}
             >
                 <Text style = {globalStyles.buttonText}>Show Modal Window</Text>
             </TouchableOpacity>
 
+        <Modal 
+            visible={showModal}
+            transparent={true}
+            onRequestClose={() => {
+                //for android. callback function called when user taps hardware back button on android
+                //on ios its called using drag gesture only if presentations style is pageSheet or formSheet
+                setShowModal(!showModal)
+            }}
+        >
+                
+                  
             <View style={ globalStyles.container}>
                 <View style={styles.modalView}>
                     <Text style={styles.modalText}>This is modal window</Text>
+                    <Text style={styles.modalText}>You can design your modal window as needed</Text>
 
                     <TouchableOpacity
                         style = {globalStyles.button}
                         onPress={() => {
                             //set the showModal to true to close modal window
+                            setShowModal(!showModal)
                         }}
                     >
                         <Text style = {globalStyles.buttonText}>Close the popup</Text>
                     </TouchableOpacity>
                 </View>
+                
             </View>
-
+        </Modal> 
         </View>
     );
 };
@@ -156,10 +245,11 @@ const styles = StyleSheet.create({
     progressText: {
         fontSize: 16,
         fontWeight: "bold",
+        color: 'magenta'
     },
     modalView: {
         width:"60%",
-        height:'60%',
+        height:'30%',
         margin: 5,
         backgroundColor: "white",
         borderColor:"black", 
@@ -167,7 +257,7 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         padding: 15,
         alignItems: "center", 
-        justifyContent: "space-between"
+        justifyContent: "space-evenly"
     }
 
 });
